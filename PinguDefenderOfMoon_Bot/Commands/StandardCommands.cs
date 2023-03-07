9bd61b35a62b3f690c4866cf890583afd51d9521
Jack Axen <knackekung@gmail.com>
2023-03-07 18:52:01 +0100

    Save

--- PinguDefenderOfMoon_Bot/Commands/StandardCommands.cs
+++ PinguDefenderOfMoon_Bot/Commands/StandardCommands.cs
@@ -10,17 +10,18 @@ namespace PinguDefenderOfMoon_Bot.Commands
 {
     public class StandardCommands : BaseCommandModule
     {
-        [Command("Ping")]
+        [Command("ryangosling")]
         public async Task TestCommand(CommandContext ctx)
         {
-            await ctx.Channel.SendMessageAsync("Pong");
+            await ctx.Channel.SendMessageAsync("Yo is that ryan gosling? https://variety.com/wp-content/uploads/2022/07/ryan-gosling-gray-man-premiere.jpg?w=999");
+            
         }
 
-        [Command("banorder")]
-        public async Task DisplayForAllMembers(CommandContext ctx)
+        [Command("add")]
+        public async Task Addition(CommandContext ctx, int num1, int num2)
         {
-
-            await ctx.Channel.SendMessageAsync("");
+            int answer = num1 + num2;
+            await ctx.Channel.SendMessageAsync(answer.ToString());
         }
     }
 }
